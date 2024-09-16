1339fc97ccce89c6be5e161e28317451285068e9
Mathis AGUADO <mathis.aguado@universite-paris-saclay.fr>
2024-09-16 15:36:40 +0200

    TP2 ex1 question 3

--- tienda/empanadas/models.py
+++ tienda/empanadas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    #cle primaire, avec auto-incrementation
+    idIngredient = models.AutoField(primary_key=True)
+    #chaine de caractere de taille bornee
+    nomIngredient = models.CharField(max_length=50)
+    #version python du toString(), utilisee par django dans ses interfaces
+    def __str__(self) :
+        return self.nomIngredient
+    
+class Empanada(models.Model):
+    #cle primaire, avec auto-incrementation
+    idEmpanada = models.AutoField(primary_key=True)
+    #chaine de caractere de taille bornee
+    nomEmpanada = models.CharField(max_length=50)
+    #nombre decima, max 6 chiffres dont 2 apres la virgule
+    prix = models.DecimalField(max_digits=6, decimal_places=2,)
+    #version python du toString(), utilisee par django dans ses interfaces
+    def __str__(self) :
+        return 'empanada '+self.nomEmpanada+' (prix:'+str(self.prix)+'€)'
