58a306523f22076498d9b1c58e745e8215e32398
Mvtos <aguadomathis@gmail.com>
2024-11-17 22:30:23 +0100

    TP11 exo1 & 2

--- tienda/empanadas/admin.py
+++ tienda/empanadas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from empanadas.models import Ingredient
+from empanadas.models import Empanada
+from empanadas.models import Composition
 # Register your models here.
+
+admin.site.register(Ingredient)
+admin.site.register(Empanada)
+admin.site.register(Composition)
