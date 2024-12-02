a072559e5058ec8d0ab7f2c501931cc77129cb18
Mvtos <aguadomathis@gmail.com>
2024-12-02 16:12:37 +0100

    TP13 exo1 q1

--- tienda/comptes/models.py
+++ tienda/comptes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TiendaUser(User):
+    image = models.ImageField(default = 'imagesUsers/default.png',
+                                upload_to= 'imageUsers/')
